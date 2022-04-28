930c25ba02424907cf5981a990ab44784564c53d
José Castellanos <jotacastellanosguz@gmail.com>
2022-04-28 12:13:47 -0500

    Deleted unused packages

--- src/com/generador_cotizacion/controller/GeneradorController.java
+++ src/com/generador_cotizacion/controller/GeneradorController.java
@@ -3,13 +3,8 @@ package com.generador_cotizacion.controller;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Objects;
 import java.util.Vector;
-import java.util.stream.Collectors;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
